7029df2f42af92cf1327358d17c9215d6ee3ace1
tjysdsg <tjy1018543509@163.com>
2020-02-29 16:22:11 +0800

    Add mnist

---
+++ mnist/__init__.py
@@ -0,0 +1,5 @@
+import sys
+
+from .config import PROJECT_ROOT_DIR
+
+sys.path.insert(0, PROJECT_ROOT_DIR)
---
+++ mnist/config.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_DIR = os.path.join(PROJECT_ROOT_DIR, 'models')
+DATA_DIR = os.path.join(PROJECT_ROOT_DIR, 'data')
